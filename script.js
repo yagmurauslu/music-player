007dd3ceeaecf02c576bf4e03d5e61a273ea2263
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-13 14:09:13 +0300

    step 37

--- script.js
+++ script.js
@@ -90,6 +90,13 @@ let userData = {
  
 };
 
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+  
+}
+
 const renderSongs = (array) => {
    const songsHTML = array.map((song)=> {
    return `

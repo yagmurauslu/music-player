ebf3ebf1ce0bb65924892e92c50072e6fb9ca1dc
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-13 17:48:50 +0300

    trying hard

--- script.js
+++ script.js
@@ -103,12 +103,6 @@ const allSongs = [
 
 // audio API
 const audio = new Audio();
-useEffect(() => {
-  window.addEventListener('touchstart', () => {
-    document.getElementById('audio').muted = false
-    document.getElementById('audio').play()
-  })
-});
 //users will be able to shuffle and delete songs
 let userData = {
  songs : [...allSongs],
@@ -116,6 +110,16 @@ let userData = {
  songCurrentTime:0
  
 };
+
+const soundEffect = new Audio();
+soundEffect.autoplay = true;
+// onClick of first interaction on page before I need the sounds
+// (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+
+// later on when you actually want to play a sound at any point without user interaction
+soundEffect.src = 'path/to/file.mp3';
+
 
 const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);

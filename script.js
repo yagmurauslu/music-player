1a5f16e148af743e23ce1f241d0535ade9b23081
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-13 14:14:21 +0300

    step 39

--- script.js
+++ script.js
@@ -94,7 +94,12 @@ const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
-  
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+
 }
 
 const renderSongs = (array) => {

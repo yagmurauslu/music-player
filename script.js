d3fe98dd42c56ad4844713c7e3bb06be1bfb46c7
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-17 14:46:10 +0300

    step 66

--- script.js
+++ script.js
@@ -124,6 +124,7 @@ const playSong = (id) => {
   }
   userData.currentSong = song;
   playButton.classList.add("playing");
+  playSong(highlightCurrentSong());
 
   audio.play();
 };
@@ -158,7 +159,13 @@ const highlightCurrentSong = () => {
   const playlistSongElements = document.querySelectorAll(".playlist-song");
   const songToHighlight = document.getElementById(
     `song-${userData?.currentSong?.id}`
-  ); 
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
 };
 
 const renderSongs = (array) => {

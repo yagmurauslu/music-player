c74ae6bfe75e557872efe6e824da4e4fa5566d59
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-17 13:46:31 +0300

    new day

--- script.js
+++ script.js
@@ -111,14 +111,6 @@ let userData = {
  
 };
 
-const soundEffect = new Audio();
-soundEffect.autoplay = true;
-// onClick of first interaction on page before I need the sounds
-// (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
-soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
-
-// later on when you actually want to play a sound at any point without user interaction
-soundEffect.src = 'path/to/file.mp3';
 
 
 const playSong = (id) => {
@@ -141,6 +133,9 @@ const pauseSong = () => {
   playButton.classList.remove("playing");
   audio.pause();
 };
+
+const playNextSong = () => {}
+
 
 const renderSongs = (array) => {
    const songsHTML = array.map((song)=> {

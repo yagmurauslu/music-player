adeee18cb17038e07453a355be42a29997716232
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-17 14:01:24 +0300

    step 55

--- script.js
+++ script.js
@@ -139,6 +139,8 @@ const playNextSong = () => {
     playSong(userData?.songs[0].id);
   }else{
     const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
   }
 };
 

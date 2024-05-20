13ecbfadbdf0035874389d116025a701c40ef67a
ALPER <icmyagmuracikgoz@gmail.com>
2024-05-20 20:49:31 +0300

    step 79

--- script.js
+++ script.js
@@ -156,7 +156,15 @@ const playPreviousSong = () =>{
    }
 };
 
-const shuffle = () => {}
+const shuffle = () => {
+  userData?.songs.sort(() => Math.random() - 0.5);
+  userData.currentSong = null;
+  userData.songCurrentTime=0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+}
 
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
